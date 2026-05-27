abd8a210d15f220e6e69c8b278796a3cffebe86f
Kachyn <65445205+Kachyn@users.noreply.github.com>
2026-05-27 09:03:37 +0300

    Create useMotorControl.js

---
+++ src/composables/useMotorControl.js
@@ -0,0 +1,25 @@
+import { ref } from 'vue';
+
+export function useMotorControl() {
+  const leftMotorSpeed = ref(0);
+  const rightMotorSpeed = ref(0);
+  const RIGHT_MOTOR_COEFFICIENT = 0.85;
+
+  function calculateSpeeds(baseSpeed, direction) {
+    if (baseSpeed < 0 || baseSpeed > 100) {
+      throw new Error('Швидкість повинна бути в межах від 0 до 100');
+    }
+
+    if (direction === 'forward') {
+      leftMotorSpeed.value = baseSpeed;
+      rightMotorSpeed.value = Math.round(baseSpeed * RIGHT_MOTOR_COEFFICIENT);
+    } else if (direction === 'stop') {
+      leftMotorSpeed.value = 0;
+      rightMotorSpeed.value = 0;
+    }
+    
+    return { left: leftMotorSpeed.value, right: rightMotorSpeed.value };
+  }
+
+  return { leftMotorSpeed, rightMotorSpeed, calculateSpeeds };
+}
